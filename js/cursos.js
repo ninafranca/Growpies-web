95f0fe2b1fdadc0e92183a00c6a155ebb0bfedd3
SantiagoVega <santi.lcdl@gmail.com>
2021-08-24 08:13:24 -0300

    Coorrección carrusel de Cards, botones, tamaño

--- js/cursos.js
+++ js/cursos.js
@@ -24,3 +24,23 @@ const applyFilter = () => {
           : ""
       );
 };
+
+$(".fa-chevron-circle-right").on("click", function (e) {
+  $(".contenedor-cards").animate(
+    {
+      scrollLeft: 700,
+    },
+    700
+  );
+  e.preventDefault();
+});
+
+$(".fa-chevron-circle-left").on("click", function (e) {
+  $(".contenedor-cards").animate(
+    {
+      scrollLeft: -700,
+    },
+    700
+  );
+  e.preventDefault();
+});
